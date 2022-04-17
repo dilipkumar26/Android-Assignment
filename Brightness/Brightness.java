6af50a7e125d0ffc674bed3e6de04ab70ea2d3bf
dilipkumar26 <67823998+dilipkumar26@users.noreply.github.com>
2022-04-17 22:39:23 +0530

    Create Brightness.java

---
+++ Brightness/Brightness.java
@@ -0,0 +1,55 @@
+package com.example.myassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class Brightness extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.brightness);
+        askPermission(this);
+
+        SeekBar seekBar = findViewById(R.id.seekbar);
+        final TextView textView = findViewById(R.id.text);
+
+        seekBar.setMax(255);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                android.provider.Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS,progress);
+                textView.setText(""+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+    public void askPermission(Context c){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(Settings.System.canWrite(c)){
+                //you have permission to write settings
+            }
+            else{
+                Intent i = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+            c.startActivity(i);
+            }
+        }
+    }
+}
