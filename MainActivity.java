c476e100610c139e3ccb2fe812e25acda17d034d
dilipkumar26 <67823998+dilipkumar26@users.noreply.github.com>
2022-04-19 22:50:05 +0530

    Update and rename Audio/Audio.java to MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,22 +1,17 @@
-package com.example.myassignment;
+package com.example.audio;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
 import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.SeekBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class Audio extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     private SeekBar seekBar;
     private AudioManager audioManager;
@@ -24,7 +19,7 @@ public class Audio extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.audio);
+        setContentView(R.layout.activity_main);
 
         seekBar = findViewById(R.id.seekBar);
         audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
@@ -35,7 +30,7 @@ public class Audio extends AppCompatActivity {
 
     public void UpButton(View view) {
         audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND);
-            seekBar.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
+        seekBar.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
         Toast.makeText(this, "Volume Up", Toast.LENGTH_SHORT).show();
     }
 
