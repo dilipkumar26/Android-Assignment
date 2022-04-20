a2d3caf0d3c0f14f9e4c7457fdd371146696d4c9
dilipkumar26 <67823998+dilipkumar26@users.noreply.github.com>
2022-04-20 21:49:20 +0530

    Update MainActivity.java

--- Wifi/MainActivity.java
+++ Wifi/MainActivity.java
@@ -6,23 +6,40 @@ import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WifiManager wifiManager;
+    Button btn_wifi_on, btn_wifi_off;
+    WifiManager wifiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+        btn_wifi_on = findViewById(R.id.btnTurnOn);
+        btn_wifi_off = findViewById(R.id.btnTurnOff);
+
+        btn_wifi_on.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                wifiManager.setWifiEnabled(true);
+                Toast.makeText(MainActivity.this, "Wifi Turn ON", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        btn_wifi_off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                wifiManager.setWifiEnabled(false);
+                Toast.makeText(MainActivity.this, "Wifi Turn OFF", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    public void WifiOnButton(View view) {
-        wifiManager.setWifiEnabled(true);
-    }
-    public void WifiOffButton(View view) {
-        wifiManager.setWifiEnabled(false);
-    }
 }
